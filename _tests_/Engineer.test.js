17bf4252fe3ecef0ef2fcc5b8edce7d1b73de1b7
kngurley <kngurley25@gmail.com>
2021-11-20 15:20:06 -0600

    add engineer testing and construct

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/Engineer");
+
+test("pulls github username from constructor", () => {
+  const github = "GitHubUser";
+  const employee = new Engineer("arg1", 10, "arg3", github);
+  expect(employee.github).toBe(github);
+});
+
+test("verify getGithub()", () => {
+    const github = "GitHubUser";
+    const employee = new Engineer("arg1", 10, "arg3", github);
+    expect(employee.getGithub()).toBe(github);
+  });
+
+test("getRole() should return \"Engineer\"", () => {
+  const role = "Engineer";
+  const employee = new Engineer("arg1", 10, "arg3", "arg4");
+  expect(employee.getRole()).toBe(role);
+});
+
