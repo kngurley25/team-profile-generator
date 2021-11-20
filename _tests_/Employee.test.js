c22b711f417977169243949f3b4bb5a97aa78665
kngurley <kngurley25@gmail.com>
2021-11-20 14:46:07 -0600

    update employee.test.js file

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,14 +1,49 @@
 const Employee = require("../lib/Employee");
 
-test("creates an employee object", () => {
-    const employee = new Employee ("John");
+test("verify Employee object", () => {
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
+});
 
-    expect(employee.name).toBe("John");
-    expect(employee.email).toBe("email");
-})
+test("pulls employee name from constructor", () => {
+    const name = "John";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+});
 
-test("creates an employee object", () => {
-    const employee = new Employee ("John");
-    
-    expect(employee.email).toBe("email");
-})
+test("pulls employee ID from constructor", () => {
+    const Id = 10;
+    const employee = new Employee("arg1", Id);
+    expect(employee.id).toBe(Id);
+});
+
+test("pulls employee email from constructor", () => {
+    const email = "email@email.com";
+    const employee = new Employee("arg1", 10, email);
+    expect(employee.email).toBe(email);
+});
+
+test("verify getName()", () => {
+    const name = "John";
+    const employee = new Employee(name);
+    expect(employee.getName()).toBe(name);
+});
+
+test("verify getId()", () => {
+    const Id = 10;
+    const employee = new Employee("arg1", Id);
+    expect(employee.getId()).toBe(Id);
+});
+
+test("verify getEmail()", () => {
+    const email = "email@email.com";
+    const employee = new Employee("arg1", 10, email);
+    expect(employee.getEmail()).toBe(email);
+});
+
+test("verify getRole()", () => {
+    const role = "Employee";
+    const employee = new Employee("arg1", 10, "arg3");
+    expect(employee.getRole()).toBe(role);
+});
+
